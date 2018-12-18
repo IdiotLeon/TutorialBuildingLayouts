918f809b8b27feeb9821e0643a6b137b93773916
Leon <yanglyu.leon.7@gmail.com>
2018-12-17 17:08:48 -0800

    demo: Adding a demo snippet that state is managed in a mix-and-match approach

--- lib/DemoExampleStateManage.dart
+++ lib/DemoExampleStateManage.dart
@@ -11,10 +11,12 @@ class AppDemoStateSelfManage extends StatelessWidget {
           title: Text('Flutter Demo'),
         ),
         body: Center(
-          // To show the example that state managed by parent widget
+          // To show the example that state is managed by parent widget
           //child: ParentWidget(),
-          // To show the example that state managed by the widget itself
-          child: StateSelfManageTapBox(),
+          // To show the example that state is managed by the widget itself
+          // child: StateSelfManageTapBox(),
+          // To show the example that state is managed in a mix-and-match approach
+          child: AnotherParentWidget(),
         ),
       ),
     );
@@ -86,8 +88,7 @@ class _ParentWidgetState extends State<ParentWidget> {
 }
 
 class ChildWidgetTapBox extends StatelessWidget {
-  ChildWidgetTapBox(
-      {Key key, this.active: false, @required this.onChanged})
+  ChildWidgetTapBox({Key key, this.active: false, @required this.onChanged})
       : super(key: key);
 
   final bool active;
@@ -115,5 +116,94 @@ class ChildWidgetTapBox extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+// ---------------- State managed by mix-and-match approach --------------------
+class AnotherParentWidget extends StatefulWidget {
+  @override
+  _AnotherParentWidgetState createState() => _AnotherParentWidgetState();
+}
+
+class _AnotherParentWidgetState extends State<AnotherParentWidget> {
+  bool _active = false;
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MixedApproachTapBox(
+        active: _active,
+        onChanged: _handleTapBoxChanged,
+      ),
+    );
+  }
+}
+
+class MixedApproachTapBox extends StatefulWidget {
+  MixedApproachTapBox({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  _StateMixApproachTapBox createState() => _StateMixApproachTapBox();
+}
+
+class _StateMixApproachTapBox extends State<MixedApproachTapBox> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This example adds a green border on tap down
+    // On tap up, the square changes to the opposite state
+    return GestureDetector(
+        onTapDown: _handleTapDown,
+        // to handle the tap events in the order that
+        onTapUp: _handleTapUp,
+        // they occur, down, up, tap, cancel
+        onTap: _handleTap,
+        onTapCancel: _handleTapCancel,
+        child: Container(
+          child: Center(
+            child: Text(widget.active ? 'Active' : 'Inactive',
+                style: TextStyle(fontSize: 32.0, color: Colors.white)),
+          ),
+          width: 200.0,
+          height: 200.0,
+          decoration: BoxDecoration(
+            color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+            border: _highlight
+                ? Border.all(color: Colors.teal[700], width: 10.0)
+                : null,
+          ),
+        ));
   }
 }
